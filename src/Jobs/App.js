aeeaf3036d9f337584c6cd7539d086dea9c4d256
Alex Brazier <aejbrazier.apps@gmail.com>
2018-07-26 21:11:02 +0100

    Add missing question to job buttons

--- src/Jobs/App.js
+++ src/Jobs/App.js
@@ -1,5 +1,5 @@
 import React, {Component} from "react"
-import {Button, ButtonGroup} from "react-bootstrap"
+import {Button, ButtonGroup, Panel, ControlLabel, ButtonToolbar} from "react-bootstrap"
 import Job from "./Job";
 import NoJob from "./NoJob";
 
@@ -21,12 +21,19 @@ export default class App extends Component {
 
     return (
       <div>
-        <ButtonGroup>
-          <Button onClick={this.handleJobCount.bind(this, 0)} className={jobCount === 0 ? "selected" : null}>0</Button>
-          <Button onClick={this.handleJobCount.bind(this, 1)} className={jobCount === 1 ? "selected" : null}>1</Button>
-          <Button onClick={this.handleJobCount.bind(this, 2)} className={jobCount === 2 ? "selected" : null}>2</Button>
-          <Button onClick={this.handleJobCount.bind(this, 3)} className={jobCount === 3 ? "selected" : null}>3</Button>
-        </ButtonGroup>
+        <Panel className="Panel">
+          <Panel.Body>
+            <ControlLabel>How many jobs do you have?</ControlLabel>
+            <ButtonToolbar>
+              <ButtonGroup>
+                <Button onClick={this.handleJobCount.bind(this, 0)} className={jobCount === 0 ? "selected" : null}>0</Button>
+                <Button onClick={this.handleJobCount.bind(this, 1)} className={jobCount === 1 ? "selected" : null}>1</Button>
+                <Button onClick={this.handleJobCount.bind(this, 2)} className={jobCount === 2 ? "selected" : null}>2</Button>
+                <Button onClick={this.handleJobCount.bind(this, 3)} className={jobCount === 3 ? "selected" : null}>3</Button>
+              </ButtonGroup>
+            </ButtonToolbar>
+          </Panel.Body>
+        </Panel>
         {this.state.jobCount === 0 ?
           <NoJob />
           :
